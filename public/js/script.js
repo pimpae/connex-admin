c5179850cbfa49f991f1b1df51678ec885e6a406
pimpae <pimpae@hotmail.com>
2023-09-30 22:12:14 +0700

    23:50 09/27/2023

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,41 @@
-const bottomSheet = document.querySelector(".modal-drag");
-const sheetOverlay = bottomSheet.querySelector(".sheet-overlay");
-const sheetContent = bottomSheet.querySelector(".modal-body");
-const dragIcon = bottomSheet.querySelector(".dis-m");
+const showModalBtn = document.querySelector(".show-modal1");
+const bottomSheet = document.querySelector(".bottom-sheet1");
+const sheetOverlay = bottomSheet.querySelector(".sheet-overlay1");
+const sheetContent = bottomSheet.querySelector(".content1");
+const dragIcon = bottomSheet.querySelector(".drag-icon1");
 let isDragging = false, startY, startHeight;
 const showBottomSheet = () => {
     bottomSheet.classList.add("show");
     document.body.style.overflowY = "hidden";
     updateSheetHeight(50);
-}
-
+} 
 const updateSheetHeight = (height) => {
-    sheetContent.style.height = `80vh`; //updates the height of the sheet content
+    sheetContent.style.height = `${height}vh`; //updates the height of the sheet content
+    
     bottomSheet.classList.toggle("fullscreen", height === 100);
-}
+} 
 const hideBottomSheet = () => {
     bottomSheet.classList.remove("show");
     document.body.style.overflowY = "auto";
-}
+} 
 const dragStart = (e) => {
     isDragging = true;
     startY = e.pageY || e.touches?.[0].pageY;
     startHeight = parseInt(sheetContent.style.height);
     bottomSheet.classList.add("dragging");
-}
+} 
 const dragging = (e) => {
     if(!isDragging) return;
     const delta = startY - (e.pageY || e.touches?.[0].pageY);
     const newHeight = startHeight + delta / window.innerHeight * 100;
     updateSheetHeight(newHeight);
-}
+} 
 const dragStop = () => {
     isDragging = false;
     bottomSheet.classList.remove("dragging");
     const sheetHeight = parseInt(sheetContent.style.height);
     sheetHeight < 25 ? hideBottomSheet() : sheetHeight > 75 ? updateSheetHeight(100) : updateSheetHeight(50);
-    sheetOverlay.addEventListener("click", hideBottomSheet); 
-    $(".modal-backdrop").removeClass("show");    
-}
-
+} 
 dragIcon.addEventListener("mousedown", dragStart);
 document.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
